2de9fd6a81f6f4d60ef26d87a4e42c6b112470f3
madfuru <100013547+madfuru@users.noreply.github.com>
2025-08-20 14:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def _psi_parse(j):
     }, None
 
 def get_pagespeed_both(url):
-    key = os.getenv("PSI_KEY")
+    key = os.getenv("AIzaSyDQSCDmfTMievyBudwEgRhaw2sbFzpEq7U")
     desk_raw = _psi_call(url, "desktop", key=key, timeout=22)
     desktop, desk_err = _psi_parse(desk_raw)
     mob_raw  = _psi_call(url, "mobile",  key=key, timeout=22)
@@ -339,6 +339,7 @@ def report():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
